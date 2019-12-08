58ea753a12f5ae4b4f121c17a168e16af6020a6c
Anastasia Pankina <siapankina@gmail.com>
2019-12-08 20:28:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+// let array = ["Ada", "Brendan", "Ali"];
+// let eventName = "birthday";
+
+function writeCards(array, eventName) {
+    let thankYouNotes = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouNotes.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYouNotes;
+}
+
+function countDown(startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber-- 
+    }
+    console.log( startingNumber );
+  }
+  
