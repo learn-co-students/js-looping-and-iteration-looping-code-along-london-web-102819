e853c424d77a0ebd6e7d63885ce42f97c3520e93
andrew-lewell <andrew.lewell@gmail.com>
2019-12-08 14:42:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const array = [];
+
+function writeCards(arr, event) {
+  for (let i = 0; i < arr.length; i++) {
+    array.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return array;
+}
+
+function countDown(count) {
+  while (count > 0) {
+    console.log(count);
+    count -= 1;
+  }
+  console.log(count);
+}
