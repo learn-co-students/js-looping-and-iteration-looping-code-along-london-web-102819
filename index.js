6653dcfb800ab78cc59057ff6385e2cd331e7353
Qing <chingwang.qw@gmail.com>
2019-12-08 09:31:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesList, occasion) {
+  let cards = []
+  for (let i = 0; i < namesList.length; i++) {
+    cards.push(`Thank you, ${namesList[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return cards
+}
+
+function countDown(max) {
+  let i = max;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
